a6e20e1b07db6d4b41354ba297c1a1ab2db6e0e5
MuhammadMasab002 <muhammadmasab2002@gmail.com>
2025-12-12 22:44:26 +0500

    integrate BestSelling in ProductDetail page

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,12 +1,15 @@
 import React, { useState } from "react";
 import ProductDetailInfo from "../components/common/products/ProductDetailInfo";
-import { useProductDetailsQuery } from "../services/api";
+import {
+  useGetFeatureProductsQuery,
+  useProductDetailsQuery,
+} from "../services/api";
 import { useParams } from "react-router-dom";
+import BestSelling from "../components/home/BestSelling";
 
 export const ProductDetail = () => {
   // get product id from route params
   const { productId } = useParams();
-  if (!productId) return;
 
   const [quantity, setQuantity] = useState(1);
 
@@ -16,33 +19,44 @@ export const ProductDetail = () => {
     isLoading,
   } = useProductDetailsQuery(productId);
   console.log("Product Details:", productDetail);
+  const { data: bestSellingProduct } =
+    useGetFeatureProductsQuery("BEST_SELLING");
+
+  if (!productId) return;
   return (
-    <div className="w-full text-black max-w-6xl mx-auto p-6 grid grid-cols-1 lg:grid-cols-2 gap-8">
-      {/* LEFT: Product Image */}
-      <div className="w-full flex justify-center">
-        <img
-          src={productDetail?.product?.image}
-          alt={productDetail?.product?.name}
-          className="w-full max-w-md rounded shadow"
+    <>
+      <div className="w-full text-black max-w-6xl mx-auto sm:px-6 py-10 grid grid-cols-1 lg:grid-cols-2 gap-8">
+        {/* LEFT: Product Image */}
+        <div className="w-full flex justify-center">
+          <img
+            src={productDetail?.product?.image}
+            alt={productDetail?.product?.name}
+            className="w-full max-w-md rounded shadow"
+          />
+        </div>
+
+        {/* RIGHT: Details Info */}
+        <ProductDetailInfo
+          title={productDetail?.product?.name}
+          // rating={4}
+          reviews={150}
+          description={
+            productDetail?.product?.description ??
+            "All black Nike sneakers, get a free legit check in the SOLESTAGE app now. "
+          }
+          price={productDetail?.product?.price}
+          colours={["bg-gray-900", "bg-red-500"]}
+          sizes={["XS", "S", "M", "L", "XL"]}
+          quantity={quantity}
+          setQuantity={setQuantity}
         />
       </div>
-
-      {/* RIGHT: Details Info */}
-      <ProductDetailInfo
-        title={productDetail?.product?.name}
-        // rating={4}
-        reviews={150}
-        description={
-          productDetail?.product?.description ??
-          "All black Nike sneakers, get a free legit check in the SOLESTAGE app now. "
-        }
-        price={productDetail?.product?.price}
-        colours={["bg-gray-900", "bg-red-500"]}
-        sizes={["XS", "S", "M", "L", "XL"]}
-        quantity={quantity}
-        setQuantity={setQuantity}
-      />
-    </div>
+      <div className="w-full h-full justify-center items-center flex flex-col gap-4 my-10">
+        <section className="w-full max-w-7xl px-3 sm:px-5 lg:py-10 md:py-8 py-6 space-y-8 sm:space-y-12">
+          <BestSelling featureProducts={bestSellingProduct?.products} />
+        </section>
+      </div>
+    </>
   );
 };
 
